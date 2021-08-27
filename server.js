1f2140ac31e94acb5d1ad476e1d5a61a4bde1b50
Mon <rocketorangemen@gmail.com>
2021-08-26 20:53:39 -0500

    react progress

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var server =http.createServer(function(req,res) {
-    res.writeHead(200, { "Content-type": "text/plain"});
-});
-
-server.listen(3001, function () {
-    console.log('Server running!')
-});
