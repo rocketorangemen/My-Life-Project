8a4ab7efa2828682a62263bc7f0bcb4bf71e8efa
Mon <rocketorangemen@gmail.com>
2021-08-30 13:49:12 -0500

    Deploy app to Heroku

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 // Creating server
+const path = require("path");
 
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Have Node serve the files for our build React app
+app.use(rexpress.static(path.resolve(__dirname, '../client/build')));
 
+// Handle GET requests to /api route
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 
