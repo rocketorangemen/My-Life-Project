3412af2f7d2a51e4b6f9a39588cb1d186df01429
Mon <rocketorangemen@gmail.com>
2021-09-09 13:51:50 -0500

    app.js debug

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -13,10 +12,7 @@ function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+      <text>hello</text>
     </div>
   );
 }
